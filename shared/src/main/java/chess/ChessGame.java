afa82f50ff11f5a1c2259d75d4ca6180fb38b717
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2024-01-13 09:08:33 -0700

    Updated files to comply with 1/13/24 emergency email

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,9 +1,7 @@
 package chess;
 
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 
 /**
  * For a class that can manage a chess game, making moves on a board
