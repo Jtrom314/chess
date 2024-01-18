685ba33670f91870e9109487da6a39f4db1a293d
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2024-01-18 13:08:00 -0700

    Phase 4 stub functions
    wrote the configureDB function

--- server/src/main/java/dataAccess/SQLDataAccess.java
+++ server/src/main/java/dataAccess/SQLDataAccess.java
@@ -4,7 +4,13 @@ import model.AuthData;
 import model.GameData;
 import model.UserData;
 
+import java.sql.SQLException;
+
 public class SQLDataAccess implements DataAccess {
+
+    public SQLDataAccess() throws DataAccessException {
+        configureDB();
+    }
     public void createUser(UserData data) throws DataAccessException{
 
     }
@@ -47,4 +53,60 @@ public class SQLDataAccess implements DataAccess {
 
     }
 
+    private final String[] createAuthTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS AuthData (
+                `authToken` varchar(256) NOT NULL,
+                `username` varchar(256) NOT NULL,
+                PRIMARY KEY (`authToken`)
+            )
+            """
+    };
+
+    private final String[] createUserTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS UserData (
+                `username` varchar(256) NOT NULL,
+                `password` varchar(256) NOT NULL,
+                `email` varchar(256) NOT NULL,
+                PRIMARY KEY (`username`)
+            )
+            """
+    };
+
+    private final String[] createGameTableStatement = {
+            """
+            CREATE TABLE IF NOT EXISTS GameData (
+                `id` int NOT NULL,
+                `whiteUsername` varchar(256) NULL,
+                `blackUsername` varchar(256) NULL,
+                `gameName` varchar(256) NOT NULL,
+                `game` TEXT NOT NULL,
+                PRIMARY KEY (`id`)
+            )
+            """
+    };
+
+    private void configureDB() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var connection = DatabaseManager.getConnection()) {
+            for (var statement : createAuthTableStatement) {
+                try (var preparedStatement = connection.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+            for (var statement: createGameTableStatement) {
+                try (var preparedStatement = connection.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+            for (var statement: createUserTableStatement) {
+                try (var preparedStatement = connection.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s",ex.getMessage()));
+        }
+    }
 }
